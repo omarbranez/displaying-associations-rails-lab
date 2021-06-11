93376c559144ea6ab981af7151da64fbbe14c16e
Omar Branez <omar.branez@gmail.com>
2021-06-11 15:05:29 -0400

    commit tree 70861f9fcb0bbd30d5d8d3eb975a658faa8dbab6

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
-
+      t.integer :artist_id, foreign_key: true
       t.timestamps null: false
     end
   end
